412faab621ca7bbc9d25ae338858fee266cc5cb4
devHksingh <hksingh.dev@gmail.com>
2026-07-28 18:06:24 +0530

    add React Query client setup and provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    // TODO: Change stale and cache time in production
+    queries: {
+      staleTime: 40 * (60 * 1000), // 40 mins
+      gcTime: 15 * (60 * 1000), // 15 mins cacheTime
+      refetchIntervalInBackground: true,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
